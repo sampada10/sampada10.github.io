e5e60619a434e0ea872e9282ffac41679d170ca1
sampada10 <63225082+sampada10@users.noreply.github.com>
2020-04-09 23:09:12 +0530

    Update samp.js

--- samp.js
+++ samp.js
@@ -1,8 +1,7 @@
+alert("Touch the cake");
 alert(" I repeate dont forget to touch the cake");
 
-window.addEventListener('click', function() {
-
+window.addEventListener("click", function() {
     var audio = document.getElementById("HBD");
     audio.play();
-
 });
